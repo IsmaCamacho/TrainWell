b2ae34208c0548b777595a1e93f893282735a8c0
IsmaCamacho <isma.cama.sanc@gmail.com>
2026-01-28 09:45:39 +0100

    ANOTACION DE COMENTARIO EN LA DATA CLASS TRAINER

--- TrainWell/app/src/main/java/com/example/trainwell/Model/Register/Trainer.kt
+++ TrainWell/app/src/main/java/com/example/trainwell/Model/Register/Trainer.kt
@@ -1,5 +1,9 @@
 package com.example.trainwell.Model.Register
 
+//MIRAR VIEWMODEL Y PENSAR SI METER MAS DETALLES QUE ESTAÍA BASTANTE BIEN SERIAN LOS CAMPOS:
+//aceptarNuevosClientes
+//cupoMaximo
+//diasSeleccionados
 data class Trainer(
     val userId:String = "",
     val biography: String,
